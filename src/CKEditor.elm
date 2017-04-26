d3b318555172e648fe8948c9c696695842f6bbbd
Jonas Schürmann <jonasschuermann@aol.de>
2017-04-26 22:49:54 +0200

    Restructure API to be more like elm-lang/html

--- src/CKEditor.elm
+++ src/CKEditor.elm
@@ -1,24 +1,30 @@
-module CKEditor exposing (ckeditor, defaultConfig)
+module CKEditor exposing (ckeditor, config, content, onCKEditorChange, defaultConfig)
 
-import Html exposing (Html, node)
+import Html exposing (node, Html, Attribute)
 import Html.Attributes exposing (property)
 import Html.Events exposing (on)
 import Json.Decode as Decode
-import Json.Encode as Encode exposing (Value, encode)
+import Json.Encode as Encode exposing (encode, Value)
 
 
-ckeditor : Value -> String -> (String -> msg) -> Html msg
-ckeditor config content msg =
-    let
-        eventDecoder =
-            Decode.field "detail" Decode.string
-    in
-        node "x-ckeditor"
-            [ property "config" config
-            , property "content" (Encode.string content)
-            , on "ckeditorchange" (Decode.map msg eventDecoder)
-            ]
-            []
+ckeditor : List (Attribute msg) -> List (Html msg) -> Html msg
+ckeditor =
+    node "x-ckeditor"
+
+
+config : Value -> Attribute msg
+config =
+    property "config"
+
+
+content : String -> Attribute msg
+content value =
+    property "content" (Encode.string value)
+
+
+onCKEditorChange : (String -> msg) -> Attribute msg
+onCKEditorChange msg =
+    on "ckeditorchange" (Decode.map msg (Decode.field "detail" Decode.string))
 
 
 defaultConfig : Value
